3a8fd6a7ebd5ad2d86cf3f7721fc37f2a777ab1f
Jennifer <67403842+jd9913@users.noreply.github.com>
2020-10-01 09:19:05 -0700

    file structure changes

---
+++ scripts/lineChart.js
@@ -0,0 +1,9 @@
+const allData = require('././data/hospital.json');
+
+
+
+
+
+let dataLineChartED = []; //array to hold daily data to generate line chart for ED patients 4/2020-current
+let dataLineChartAd = []; //array to hold daily data to generate line chart for Admit 4/2020-current
+let dataLineChartInp = []; //array to hold daily data to generate line chart for Inpatients 4/2020-current
