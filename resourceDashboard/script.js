27efe600a6d92348436e82cce783a56e7d185389
Jennifer Schlierman <jd9913@gmail.com>
2021-02-26 15:20:33 -0700

    update link

--- resourceDashboard/script.js
+++ resourceDashboard/script.js
@@ -1,7 +1,5 @@
-const site_url = "/Covid-Utilization-Dashboard";
-
 const getData = () => {
-	$.getJSON(site_url + "/data/hospital.json", (hospitalData) => {
+	$.getJSON("/data/hospital.json", (hospitalData) => {
 		let hospitalDataClean = Object.values(hospitalData);
 		console.log(hospitalData);
 		console.log(hospitalDataClean);
